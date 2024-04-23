b08770fd487316374a9f0adeee4e77599559efb0
takacsbalinttt <balint.takacs5@stud.uni-corvinus.hu>
2024-04-23 21:04:04 +0200

    teszt elott

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    internal class AccountControllerTestFixture
+    {
+        [
+     Test,
+     TestCase("abcd1234", false),
+     TestCase("irf@uni-corvinus", false),
+     TestCase("irf.uni-corvinus.hu", false),
+     TestCase("irf@uni-corvinus.hu", true)
+         ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new Controllers.AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+
+
+    }
+}
